e60d8e73883814fd3466330c258d057f0c7f1060
Rui Lopes <rgl@ruilopes.com>
2021-01-31 12:32:14 +0000

    always show the result of whoami.exe even when it fails

--- Whoami.cs
+++ Whoami.cs
@@ -10,7 +10,11 @@ namespace whoami
         {
             var result = await Cli.Wrap(@"C:\Windows\system32\whoami.exe")
                 .WithArguments("-all")
+                .WithValidation(CommandResultValidation.None)
                 .ExecuteBufferedAsync();
+            if (result.ExitCode != 0) {
+                return $"ERROR: Failed to execute whoami.exe.\nexit code:\n{result.ExitCode}\nstdout:\n{result.StandardOutput}\nstderr:\n{result.StandardError}";
+            }
             return result.StandardOutput;
         }
     }
